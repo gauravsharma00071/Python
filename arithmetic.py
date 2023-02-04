3ac5288d2d9c38f0f03df041f3905aa82580aa72
Gaurav Sharma <118480493+gauravsharma00071@users.noreply.github.com>
2023-02-04 10:59:55 +0530

    arthmatic opr

---
+++ arithmetic.py
@@ -0,0 +1,18 @@
+# program for arithmetic sum of two numbers
+
+a = eval(input("Enter first number : "))
+b = eval(input("Enter second number : "))
+print(a)
+print(b)
+
+print("addition of two number ")
+print(a+b)
+
+print("Subtraction of two number ")
+print(a-b)
+
+print("Multiplicatin of two numbers")
+print(a*b)
+
+print("Division of two numbers ")
+print(a/b)
