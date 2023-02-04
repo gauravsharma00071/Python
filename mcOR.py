ac641c94856d4c7c8fe0b62c6629894fba0fece1
Gaurav Sharma <118480493+gauravsharma00071@users.noreply.github.com>
2023-02-04 09:24:03 +0530

    McCulloch pits model  OR

---
+++ mcOR.py
@@ -0,0 +1,14 @@
+a1 = int(input("Enter the first number : "))
+a2 = int(input("Enter the second number : "))
+print(a1,a2)
+w1 = 1
+w2 = 1
+
+th = 1
+
+
+thr = a1*w1 + a2*w2
+if(thr>=th):
+    print(1)
+else:
+    print(0)
